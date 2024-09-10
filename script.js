222d4083dc70e0ad5e155256fd45337076162a71
FeliCres7 <48242703@est.ort.edu.ar>
2024-09-10 12:03:54 -0300

    hicimos la conexión con back end

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+//menu desplegable en en header
 document.getElementById('menubutton').addEventListener('click', function() {
     var menu = document.getElementById('menu');
     if (menu.style.display === 'block') {
@@ -6,3 +8,51 @@ document.getElementById('menubutton').addEventListener('click', function() {
         menu.style.display = 'block';
     }
 });
+
+const btn = document.getElementById("btnCheck")
+const mainCards = document.getElementsByClassName("lista-misprofesores")
+
+function CreateProfCard (prof) {
+    return `
+    <div class="profesor-misprofesores">
+    <p class="name-profesor">${prof.nombre} ${prof.apellido}</p>
+    <div class="content-profesor"><img src="" alt="Martín Melman">
+        <div class="datos-profesor">
+            <div class="datacontainer">
+                <p class="bold">Materia:</p>
+                <p> ${prof.materias} </p>
+            </div>
+            <div class="datacontainer">
+                <p class="bold">Días:</p>
+                <p> Lunes </p>
+            </div>
+            <div class="datacontainer">
+                <p class="bold">Horarios:</p>
+                <p> 13:30-14:30</p>
+            </div>
+        </div>
+        <div class="botones-profesor">
+            <button>Materiales de trabajo</button>
+            <button>Ir a perfil</button>
+            <button>Enviar Mensaje</button>
+        </div>
+        <div class="valoracion-profesor">
+            <p>${prof.valoracion}</p>
+        </div>
+    </div>
+</div>`
+}
+
+
+btn.addEventListener("click", async () => {
+    fetch("https://learn-hub-eta.vercel.app/profesores")
+    .then(res => res.json())
+    .then((data) => {
+        console.log(data)
+        mainCards.innerHTML = ""
+        data.forEach((profesor) => {
+            const item = CreateProfCard(profesor)
+            mainCards.innerHTML += item
+        })
+    })
+}) 
