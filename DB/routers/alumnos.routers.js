f2f4bb3fa176bc62caa440901c15147045430076
Ezequielagradnik <163456723+Ezequielagradnik@users.noreply.github.com>
2024-09-17 12:12:30 -0300

    cambie las rutas al file routersalumnos

---
+++ DB/routers/alumnos.routers.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import alumnos from './controllers/Alumnos.js';
+
+//Alumnos
+router.get('/Alumnos', alumnos.getalumnos);
+router.get('/Alumnos/:ID', alumnos.getalumnobyID);
+router.post('/Alumnos', alumnos.createAlumno);
+router.put('/Alumnos/ID', alumnos.updateAlumno);
+router.delete('/Alumnos/:ID', alumnos.deleteAlumno);
+//router.get('/Alumnos/:ID/clasebyalumno/:IDclases/',alumnos.getclasebyalumno);
+router.get('/Alumnos/:ID/perfilalumno',alumnos.getperfilalumno)
+
