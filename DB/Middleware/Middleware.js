b446e52de8b9237160e83c4e4d75748bcf9ce4fe
Ezequielagradnik <163456723+Ezequielagradnik@users.noreply.github.com>
2024-09-25 10:24:58 -0300

    feat: import all controllers in midd

---
+++ DB/Middleware/Middleware.js
@@ -0,0 +1,71 @@
+import jwt from "jsonwebtoken";
+import alumnos from "../controllers/alumnos.js";
+import profesores from "../controllers/profesores.js";
+import auth from "../controllers/auth.js";
+import clases from "../controllers/clases.js";
+import materia from "../controllers/Materia.js";
+import material from "../controllers/Material.js";
+import reservaciones from "../controllers/reservaciones.js";
+
+
+export const verifyToken = async (req, res, next) => {
+    const headerToken = req.headers['authorization'];
+
+    // Verificar si el token está proporcionado
+    if (!headerToken) {
+        return res.status(400).json({ message: "Token necesario" });
+    }
+
+    // Verificar si el token está en el formato correcto
+    const tokenParts = headerToken.split(' ');
+    if (tokenParts[0] !== 'Bearer' || tokenParts.length !== 2) {
+        return res.status(400).json({ message: "Formato del token no válido" });
+    }
+
+    const token = tokenParts[1];
+
+    try {
+        const secret = "Vamos Racing";
+        const decoded = jwt.verify(token, secret);
+
+        // Verificar si el token contiene un ID de usuario válido
+        const id = decoded.id;
+        
+        // Intentar encontrar al usuario como alumno
+        let usuario = await alumnos.findById(id);
+        
+        // Si no se encuentra como alumno, intentar encontrar como profesor
+        if (!usuario) {
+            usuario = await profesores.findById(id);
+        }
+
+        if (!usuario) {
+            return res.status(400).json({ message: "ID no válido" });
+        }
+
+        // Adjuntar el ID de usuario y el rol al objeto de la solicitud
+        req.id = id;
+        req.role = usuario.role; // Suponiendo que 'role' indica si el usuario es alumno o profesor
+        next();
+    } catch (error) {
+        return res.status(401).json({ error: "Token no válido o expirado" });
+    }
+};
+
+export const verifyAdmin = async (req, res, next) => {
+    const id = req.id;
+
+    // Recuperar detalles del usuario para verificar si es un administrador (profesor)
+    const usuario = await profesores.findById(id); // Solo verificar en profesores
+
+    if (!usuario) {
+        return res.status(404).json({ message: "Usuario no encontrado" });
+    }
+
+    // Verificar si el usuario es un administrador (profesor)
+    if (usuario.role === 'admin') { // Suponiendo que el rol 'admin' está asignado a los profesores
+        next();
+    } else {
+        return res.status(403).json({ message: "Acceso prohibido" });
+    }
+};
