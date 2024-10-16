d4825173b8b99a650614a36266ded63f30425b7d
FeliCres7 <48242703@est.ort.edu.ar>
2024-10-16 13:54:08 -0300

    .

--- DB/controllers/auth.js
+++ DB/controllers/auth.js
@@ -57,7 +57,7 @@ const login = async (req, res) => {
 };
 
 const register = async (req, res) => {
-  const { nombre, apellido, email, contraseña, confirmarContraseña, tipoUsuario } = req.body;
+  const { nombre, apellido, email, contraseña, confirmarContraseña, tipoUsuario } = req.body; 
 
   // Validaciones de campos requeridos
   if (!nombre || !apellido || !email || !contraseña || !tipoUsuario) {
