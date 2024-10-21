51db86881870b5e9be22f3a7e0ccae8ea2f2c014
Ezequiel Agradnik <ezequielagradnik@gmail.com>
2024-10-21 10:14:47 -0300

    ya se puede registrar

--- DB/upload.js
+++ DB/upload.js
@@ -1,5 +1,8 @@
 import { v2 as cloudinary } from 'cloudinary';
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+
 cloudinary.config({
     cloud_name: "dirlzvaqd",
     api_key: "319517129175242",
